28700083c4c62030ef1e475a09d76f3eb399c86d
Thomas Goos <goost15@gmail.com>
2018-12-03 00:48:24 +0100

    day 1 + day 2

---
+++ src/day01/mod.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::env;
+use core::borrow::Borrow;
+
+pub fn part1() {
+    let input = BufReader::new(File::open("input/day01a").unwrap()).lines();
+    let result = input.into_iter().fold(0, |sum, val| { sum + val.unwrap().parse::<isize>().unwrap() });
+    println!("{}", result);
+}
+
+pub fn part2() {
+    let input = BufReader::new(File::open("input/day01a").unwrap()).lines();
+    let values: Vec<String> = input.into_iter().map(|a| a.unwrap()).collect();
+
+    let mut values_reached = Vec::new();
+    let mut acc = 0;
+
+    loop {
+        for line in &values {
+            if values_reached.contains(acc.borrow()) {
+                println!("{} reached twice first", acc);
+                return;
+            } else {
+                values_reached.push(acc);
+            }
+            acc += line.parse::<i32>().unwrap();
+        }
+    }
+}
---
+++ src/day02/mod.rs
@@ -0,0 +1,52 @@
+use levenshtein::levenshtein;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use core::borrow::Borrow;
+use std::collections::HashMap;
+
+pub fn part1() {
+    let input = BufReader::new(File::open("input/day02a").unwrap()).lines();
+    //let input = vec!["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"];
+
+    let mut twice = 0;
+    let mut thrice = 0;
+
+    for line in input.map(|a| a.unwrap()) {
+        let mut letters: HashMap<char, i32> = HashMap::new();
+
+        for c in line.chars().into_iter() {
+            if letters.contains_key(c.borrow()) {
+                let val = letters.get_mut(c.borrow()).unwrap();
+                *val += 1;
+            } else {
+                letters.insert(c, 1);
+            }
+        }
+
+        twice += if letters.iter().any(|l| *l.1 == 2) { 1 } else { 0 };
+        thrice += if letters.iter().any(|l| *l.1 == 3) { 1 } else { 0 };
+    }
+
+    println!("checksum {}", twice * thrice);
+}
+
+pub fn part2() {
+    let input = BufReader::new(File::open("input/day02a").unwrap()).lines();
+    let mut words : Vec<String> = Vec::new();
+
+    for line in input.map(|a| a.unwrap()) {
+        for word in words.iter() {
+            if levenshtein(word.as_str(), line.as_ref()) == 1 {
+                for c in line.chars().zip(word.chars()).into_iter() {
+                    if c.0 == c.1 {
+                        print!("{}", c.0);
+                    }
+                }
+            }
+        }
+
+        words.push(line);
+    }
+}
