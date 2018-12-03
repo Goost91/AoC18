69ce97486c9709a412a69054807be05a4e59d7a1
Thomas Goos <goost15@gmail.com>
2018-12-03 18:29:15 +0100

    parse day2b using an iter chain

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -15,9 +15,9 @@ pub fn part1() {
     for line in input.map(|a| a.unwrap()) {
         let mut letters: HashMap<char, i32> = HashMap::new();
 
-        for c in line.chars().into_iter() {
-            *letters.entry(c).or_insert(1) += 1;
-        }
+        line.chars()
+            .into_iter()
+            .for_each(|c| *letters.entry(c).or_insert(0) += 1);
 
         twice += if letters.iter().any(|l| *l.1 == 2) { 1 } else { 0 };
         thrice += if letters.iter().any(|l| *l.1 == 3) { 1 } else { 0 };
@@ -28,16 +28,19 @@ pub fn part1() {
 
 pub fn part2() {
     let input = BufReader::new(File::open("input/day02a").unwrap()).lines();
-    let mut words : Vec<String> = Vec::new();
+    let mut words: Vec<String> = Vec::new();
 
     for line in input.map(|a| a.unwrap()) {
         for word in words.iter() {
             if levenshtein(word.as_str(), line.as_str()) == 1 {
-                for c in line.chars().zip(word.chars()).into_iter() {
-                    if c.0 == c.1 {
-                        print!("{}", c.0);
-                    }
-                }
+                line.chars()
+                    .zip(word.chars())
+                    .into_iter()
+                    .for_each(|c| {
+                        if c.0 == c.1 {
+                            print!("{}", c.0);
+                        }
+                    });
             }
         }
 
